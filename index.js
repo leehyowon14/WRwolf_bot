6d61aa5d4db33d22855e77acade6e9252ff77a56
leehyowon14 <kingtv347@gmail.com>
2020-11-07 21:20:40 +0900

    help fix

--- index.js
+++ index.js
@@ -425,7 +425,7 @@ client.on('message', async message => {
   if(message.content == 'w_help') {
 	let embed = new Discord.RichEmbed()
 	  .setTitle('울프봇 명령어')
-	  .color(0x82CAFA)
+	  .setColor(0x82CAFA)
       .setThumbnail(img)
       .addBlankField()
 	  .addField('이쉬/이쒸', '이쒸')
