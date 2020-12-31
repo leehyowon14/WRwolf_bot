62389e4653a30514880e24600a0348b68c89a74b
leehyowon14 <kingtv347@gmail.com>
2020-12-31 22:47:14 +0900

    add music

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
 require("dotenv").config();//Loading .env
 const Discord = require('discord.js');
 const fs = require("fs");
-const { Client, Util} = require('discord.js');
 const { Collection } = require("discord.js");
 const request = require("request")
 const client = new Discord.Client();
 const token = process.env.token;
+const DisTube = require('distube')
 const webhook = new Discord.WebhookClient(process.env.webhookid, process.env.webhooktoken);
 const prefix = '-'
+const config = require("./config.json")
 client.commands = new Collection();//Making client.commands as a Discord.js Collection
 client.queue = new Map()
 
+client.config = require("./config.json")
+client.distube = new DisTube(client, { searchSongs: true, emitNewSongOnly: true, leaveOnFinish: true })
+client.aliases = new Discord.Collection()
+client.emotes = config.emoji;
+
+fs.readdir("./commands/", (err, files) => {
+  let jsFiles = files.filter(f => f.split(".").pop() === "js")
+  if (jsFiles.length <= 0) return console.log("Could not find any commands!")
+  jsFiles.forEach((file) => {
+      let cmd = require(`./commands/${file}`)
+      console.log(`Loaded ${file}`)
+      client.commands.set(cmd.name, cmd)
+      if (cmd.aliases) cmd.aliases.forEach(alias => client.aliases.set(alias, cmd.name))
+  })
+})
 
 client.on('ready', async () => {
   console.log('WRwolf_bot is now online');
   client.user.setPresence({ activity: { name: '명령어:w_help' }, status: 'online'})
 });
 
-client.on('message', message => {
+client.on('message', async message => {
+
+  if (!message.content.startsWith(prefix)) return
+  const args = message.content.slice(prefix.length).trim().split(/ +/g)
+  const command = args.shift().toLowerCase();
+  let cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
+  if (!cmd) return
+  try {
+      cmd.run(client, message, args)
+  }
+  catch (e) {
+      console.error(e)
+      message.reply("Error: " + e)
+  }
+
   if (message.content == "ping") {
     return message.reply("pong")
   }
@@ -242,5 +272,28 @@ client.on('messageDelete', async message => {
   webhook.send(embed)
 
 })
+
+const status = (queue) => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode == 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
+client.distube
+    .on("playSong", (message, queue, song) => message.channel.send(
+        `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${status(queue)}`
+    ))
+    .on("addSong", (message, queue, song) => message.channel.send(
+        `${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+    ))
+    .on("${client.emotes.play} |playList", (message, queue, playlist, song) => message.channel.send(
+        `${client.emotes.play} | Play \`${playlist.title}\` playlist (${playlist.total_items} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
+    ))
+    .on("addList", (message, queue, playlist) => message.channel.send(
+        `${client.emotes.success} | Added \`${playlist.title}\` playlist (${playlist.total_items} songs) to queue\n${status(queue)}`
+    ))
+    // DisTubeOptions.searchSongs = true
+    .on("searchResult", (message, result) => {
+        let i = 0;
+        message.channel.send(`**Choose an option from below**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`);
+    })
+    // DisTubeOptions.searchSongs = true
+    .on("searchCancel", (message) => message.channel.send(`${client.emotes.error} | Searching canceled`))
+    .on("error", (message, err) => message.channel.send(`${client.emotes.error} | An error encountered: ${err}`));
 
 client.login(token);
